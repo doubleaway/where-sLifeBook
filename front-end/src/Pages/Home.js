70e583acccee088695e5948678e779764cac9680
DESKTOP-92SISGK\yangcharin <ycl042138@gmail.com>
2024-02-12 23:47:10 +0900

    react-slick 추가(아직 조정 전)

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,7 +1,43 @@
 import React from "react";
 import "../Style/Css/home.css";
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const Home = () => {
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
     return (
         <section className="homeSec">
             <div className="introImage">
@@ -10,6 +46,34 @@ const Home = () => {
                     <p>당신의 어플리케이션</p>
                     <p className="title">Where's Life Book</p>
                 </div>
+            </div>
+            <div className="slider-container">
+                <Slider {...settings}>
+                    <div className="item_1">
+                        <h3>1</h3>
+                    </div>
+                    <div className="item_1">
+                        <h3>2</h3>
+                    </div>
+                    <div className="item_1">
+                        <h3>3</h3>
+                    </div>
+                    <div className="item_1">
+                        <h3>4</h3>
+                    </div>
+                    <div className="item_1">
+                        <h3>5</h3>
+                    </div>
+                    <div className="item_1">
+                        <h3>6</h3>
+                    </div>
+                    <div className="item_1">
+                        <h3>7</h3>
+                    </div>
+                    <div className="item_1">
+                        <h3>8</h3>
+                    </div>
+                </Slider>
             </div>
         </section>
     )
